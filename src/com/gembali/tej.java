d708b2e00c8ca3f4b18d372871c752123da01c4e
GembaliChaitanya <gembalichaitanya555@gmail.com>
2018-04-18 23:36:26 +0530

    Initial commit

---
+++ src/com/gembali/tej.java
@@ -0,0 +1,19 @@
+package com.gembali;
+
+public class tej {
+    private String name = "gembali";
+    int richness = 60000000;
+
+    void ram() {
+        System.out.println(richness);
+    }
+
+    String sita() {
+        return name;
+    }
+
+    int area(int length, int breadth) {
+        return length * breadth;
+    }
+
+}
